0efc59a630d991b252b19f7610427980509bb1a4
Ilia Belov <belov38@gmail.com>
2024-08-29 22:13:26 +1200

    check css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="ru">
     <head>
         {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-        <script src="https://telegram.org/js/telegram-web-app.js"></script>
+        <script src="https://telegram.org/js/telegram-web-app.js" />
     </head>
     <body className={inter.className}>{children}</body>
     </html>
