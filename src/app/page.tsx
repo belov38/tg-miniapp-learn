ccf060c917035d1116c894cc0e96a3af9dea8f41
Ilia Belov <belov38@gmail.com>
2024-08-29 22:36:11 +1200

    styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,13 @@ import {useTelegram} from "@/providers/telegram-provider";
 
 export default function Home() {
     const {webApp, user} = useTelegram()
+
     webApp?.MainButton.show();
+    webApp?.initDataUnsafe
     return (
         <main>
             <h1>TG WEB App</h1>
+            <div>{user?.id}</div>
         </main>
     );
 }
