43769ce6e3f7f58929ac49d4d6b5165f3def9b9f
userc <belov38@gmail.com>
2024-08-31 12:50:24 +1200

    new example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,41 @@
-'use client'
-import {AppRoot, List} from '@telegram-apps/telegram-ui';
-import {useTelegram} from "@/providers/telegram-provider";
-import {Button} from '@telegram-apps/telegram-ui';
-import {useState} from "react";
+'use client';
+
+import { Section, Cell, Image, List } from '@telegram-apps/telegram-ui';
+
+import { Link } from '@/components/Link/Link';
+
+import tonSvg from './_assets/ton.svg';
 
 export default function Home() {
-    const {webApp, user} = useTelegram()
-
-    const [contact, setContact] = useState("---")
-    webApp?.enableClosingConfirmation();
-    webApp?.BackButton.show();
-    webApp?.SettingsButton.show();
-    webApp?.BackButton.onClick(() => {
-        webApp?.BackButton.hide();
-        webApp?.sendData('/somecommand')
-    });
-    webApp?.MainButton.show();
-    webApp?.MainButton.onClick(() => {
-        webApp?.sendData('/somecommand2')
-    });
-    return (
-        <AppRoot>
-            <main>
-                <h1>TG WEB App</h1>
-                <pre>{JSON.stringify(user, null, 2)}</pre>
-                <div className="card">
-                    <button onClick={() => webApp?.showAlert("Ура")}>Alert</button>
-                </div>
-                <div>
-                    <button onClick={() => webApp?.showPopup({
-                        title: "Pop up",
-                        message: "hello world",
-
-                    }, () => {
-                        webApp?.requestContact((success, res)=>{
-                            if(success){
-                                setContact(JSON.stringify(res, null, 2))
-                            }
-                        })
-                    })}>Pop up
-                    </button>
-                </div>
-                <Button onClick={()=>{setContact('???')}}>Click me</Button>
-                <div>{contact}</div>
-                <button onClick={async ()=>{
-                    await fetch('https://webhook.site/2d9371ba-544c-4e50-8908-095f92ec863e')
-                }}>Fetch</button>
-            </main>
-        </AppRoot>
-    );
+  return (
+    <List>
+      <Section
+        header='Features'
+        footer='You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects'
+      >
+        <Link href='/ton-connect'>
+          <Cell
+            before={<Image src={tonSvg.src} style={{ backgroundColor: '#007AFF' }}/>}
+            subtitle='Connect your TON wallet'
+          >
+            TON Connect
+          </Cell>
+        </Link>
+      </Section>
+      <Section
+        header='Application Launch Data'
+        footer='These pages help developer to learn more about current launch information'
+      >
+        <Link href='/init-data'>
+          <Cell subtitle='User data, chat information, technical data'>Init Data</Cell>
+        </Link>
+        <Link href='/launch-params'>
+          <Cell subtitle='Platform identifier, Mini Apps version, etc.'>Launch Parameters</Cell>
+        </Link>
+        <Link href='/theme-params'>
+          <Cell subtitle='Telegram application palette information'>Theme Parameters</Cell>
+        </Link>
+      </Section>
+    </List>
+  );
 }
